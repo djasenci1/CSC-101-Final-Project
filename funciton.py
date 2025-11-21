a53d2680aeb3ef564243cbdd041d2d39dea8a57e
dougl <djasenci@calpoly.edu>
2025-11-21 15:03:30 -0800

    First Commit!

--- funciton.py
+++ funciton.py
@@ -6,12 +6,12 @@ import main
 # Ex Input: Person[iphone, 5] it takes 5 hours for this Person to charge their phones
 # Ex Output: Compares it to the total_charging_time of the device
 
-def phone_charging_time(dict_phone: dict[str, float]) -> list[float]:
+def phone_charging_time(dict_phone: dict[str, float], phone_key: str) -> list[float]:
     new1 = []
     for x in dict_phone:
         new1.append(dict_phone[x])
-    if new1 < main.Person[phone_charging_time]: #This is the five row on the Person Object calling the total_phone_charging_time in the test_file
-        total = main.Person[phone_charging_time]
+    if new1 < main.Person[phone_key]: #This is the five row on the Person Object calling the total_phone_charging_time in the test_file
+        total = main.Person[phone_key]
         total_1 = (new1 / total) * 100  # This gets the percentage
         print("Person uses less electricity consumption on phone by", new1)
     else:
