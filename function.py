00e07997e089130020a34c0739b35d965fa8181e
dougl <djasenci@calpoly.edu>
2025-11-25 19:41:00 -0800

    First Commit!

--- function.py
+++ function.py
@@ -52,6 +52,20 @@ def person_laptop_highest_kilo_wattage(list1, value):
 
     return new1
 
+# This function, when given survey_data, returns people with the lowest kilo wattage ipad depend on if it's less than 11 kilo wattage (value)
+# Intput: survey_data -> list1 and value -> 11 kilo wattage
+# Output: A new dictorary of people with the lowest kilo wattage with they phone kilo_wattage displayed
+def person_ipad_lowest_kilo_wattage(list1, value):
+    new1 = []
+
+    for i in range(0, len(list1)):
+        if list1[i].ipad < value:
+            new1.append(list1[i].name)
+            new1.append(list1[i].ipad)
+
+    return new1
+
+
 # This function converts minutes to hours
 def conversion_to_hour(number):
     conversion = number // 60 + number % 60 / 60
@@ -102,25 +116,3 @@ def testing_results(list1, result_messages):
     return the_result
 
 
-# This function summarizes the survey results
-def summary(list1):
-    total_watts = all_wattage(total_wattage_used(list1))
-    total_cost = total_costs_in_a_year(total_watts)
-
-    overusers = []
-    for p, watts in zip(list1, total_wattage_used(list1)):
-        if watts > 1500:
-            overusers.append(p.name)
-
-    verdict = (
-        "Number of Calpoly Dormmates in Survey:", len(list1),
-        "Total amount of electricity that they used:", total_watts,
-        "total_cost_year", total_cost,
-        "Final Verdict=",
-        "Most of the students are not overusing their electricity",
-        "However",
-        overusers,
-        "needs to cut down on their spending"
-    )
-
-    return verdict
